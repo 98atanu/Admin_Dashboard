bc005fd7284e5b9b249fb4ffb8ba696eb194f68e
atanu98 <chakrabortyatanu238.com>
2025-02-06 11:26:23 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import { ColorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from './scenes/global/Topbar';
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import { Route, Routes } from 'react-router-dom';
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Form from './scenes/form';
+// import Bar from "./scenes/bar";
 
-function App() {
-  const [count, setCount] = useState(0)
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+import Calender from "./scenes/calender/calender"
 
+const App = () => {
+
+  const [theme, colorMode] = useMode()
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className='app'>
+          <Sidebar/>
+          <main className='content'>
+            <Topbar/>
+            <Routes>
+              <Route path= "/" element={<Dashboard/>}/>
+              <Route path= "/team" element={<Team/>}/>
+              <Route path= "/contacts" element={<Contacts/>}/>
+              <Route path= "/invoices" element={<Invoices/>}/>
+              {/* <Route path= "/bar" element={<Bar/>}/> */}
+              <Route path= "/form" element={<Form/>}/>
+              {/* <Route path= "/line" element={<Line/>}/> */}
+              {/* <Route path= "/pie" element={<Pie/>}/> */}
+              {/* <Route path= "/faq" element={<FAQ/>}/> */}
+              {/* <Route path= "/geography" element={<Geography/>}/> */}
+              <Route path= "/calender" element={<Calender />}/>
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
